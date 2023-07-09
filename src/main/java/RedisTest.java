42cb303fd2fbb061437af69a287eece9e4efbc33
Serj <serj053@yandex.ru>
2023-07-09 10:51:08 +0300

    last commit

--- src/main/java/RedisTest.java
+++ src/main/java/RedisTest.java
@@ -9,43 +9,37 @@ import java.util.Random;
 
 public class RedisTest {
     public static void main(String[] args) throws InterruptedException {
+
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         RedissonClient redisson = Redisson.create(config);
         RScoredSortedSet<String> sortedSet = redisson.getScoredSortedSet("users");
+
         for (int i = 0; i < 20; i++) {
-            sortedSet.add(i, String.valueOf(i + 1));
-            // sortedSet.add(new Date().getTime(), String.valueOf(i + 1));
+            sortedSet.add(new Date().getTime(), String.valueOf(i + 1));
         }
+
         Random random = new Random();
         int user_insert_point = random.nextInt(1, 11);
         boolean flag = true;
         Iterator<String> element = sortedSet.iterator();
         while (element.hasNext()) {
-            //  System.out.println("user_insert_point -" + user_insert_point + "  flag -" + flag);
             String elem = sortedSet.pollFirst();
             if (flag && user_insert_point == Integer.parseInt(elem)) {
-                //      System.out.println("before 10");
                 int user_paid = random.nextInt(1, 21);
                 System.out.println("блатной пользователь " + user_paid + "  оплатил услугу");
-                //       sortedSet.removeRangeByRank(user_paid, user_paid);
                 sortedSet.add(new Date().getTime(), String.valueOf(user_paid));
                 user_insert_point = random.nextInt(10, 20);
                 flag = false;
             } else if (!flag && user_insert_point == Integer.parseInt(elem)) {
-                //          System.out.println("before 20");
                 int user_paid = random.nextInt(1, 21);
                 System.out.println("блатной пользователь " + user_paid + "  оплатил услугу");
-                //       sortedSet.removeRangeByRank(user_paid, user_paid);
                 sortedSet.add(new Date().getTime(), String.valueOf(user_paid));
                 user_insert_point = random.nextInt(1, 11);
                 flag = true;
             }
-            //        else {
             System.out.println(" На главной странице показываем пользователя  " + elem);
             sortedSet.add(new Date().getTime(), elem);
-            //      }
-
             Thread.sleep(1000);
         }
         sortedSet.removeRangeByRank(0, -1);
